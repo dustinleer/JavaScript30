a016dc7691cd4366c3ab98e334cdaf51ee22dd6a
Dustin Leer <dustin@wildcanon.com>
2018-10-11 15:43:55 -0400

    Removes console logs and additional whitespace

--- 01 - JavaScript Drum Kit/scripts.js
+++ 01 - JavaScript Drum Kit/scripts.js
@@ -2,13 +2,8 @@
  * Call functions on Window Event Listener
 ********************************************/
 function playSound(e) {
-	// console.log(e.keyCode);
-	
-	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-	// console.log(audio);
-	
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);	
 	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-	// console.log(key);
 	
 	// stop the function from running all together
 	if (!audio) return;
@@ -22,13 +17,8 @@ function playSound(e) {
 }
 
 function removeTransition(e) {
-	// console.log(e);
-	
 	// skip if it's not a transform
 	if(e.propertyName !== 'transform') return;
-	
-	// console.log(e.propertyName);
-	
 	this.classList.remove('playing');
 }
 
