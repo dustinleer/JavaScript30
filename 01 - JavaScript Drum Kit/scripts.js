301894af889987dcef46c6de659ae308df8daa5c
Dustin Leer <dustin@wildcanon.com>
2018-10-11 15:41:00 -0400

    Finishes JS
    Cleans up original function into playSound function
    
    Adds transitions for when the keys are hit

--- 01 - JavaScript Drum Kit/scripts.js
+++ 01 - JavaScript Drum Kit/scripts.js
@@ -1,17 +1,38 @@
-window.addEventListener('keydown', function(e) {
+/********************************************
+ * Call functions on Window Event Listener
+********************************************/
+function playSound(e) {
 	// console.log(e.keyCode);
 	
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-	
 	// console.log(audio);
+	
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	// console.log(key);
 	
 	// stop the function from running all together
 	if (!audio) return;
-
+	
 	// Rewinds audio file to start
 	audio.currentTime = 0;
-
+	
 	// Plays audio file
 	audio.play();
+	key.classList.add('playing');
+}
 
-});
+function removeTransition(e) {
+	// console.log(e);
+	
+	// skip if it's not a transform
+	if(e.propertyName !== 'transform') return;
+	
+	// console.log(e.propertyName);
+	
+	this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
