027ad71f343798e70dc57f0ca6c59aa505736f15
Dustin Leer <dustin@wildcanon.com>
2018-10-12 10:07:24 -0400

    Adds JS file to make clockwork

--- 02 - JS and CSS Clock/scripts.js
+++ 02 - JS and CSS Clock/scripts.js
@@ -1,28 +1,27 @@
 /********************************************
  * Call functions on Window Event Listener
 ********************************************/
-function playSound(e) {
-	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);	
-	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-	
-	// stop the function from running all together
-	if (!audio) return;
-	
-	// Rewinds audio file to start
-	audio.currentTime = 0;
-	
-	// Plays audio file
-	audio.play();
-	key.classList.add('playing');
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    // console.log(seconds);
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+    // console.log(minutes);
+
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    // console.log(hours);
+
 }
-
-function removeTransition(e) {
-	// skip if it's not a transform
-	if(e.propertyName !== 'transform') return;
-	this.classList.remove('playing');
-}
-
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-
-window.addEventListener('keydown', playSound);
+setInterval(setDate, 1000);
